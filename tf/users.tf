86d4f1aa09e905ec1e9b8c0ec1c4ce45802dff3e
Tom Hall <thattommyhall@gmail.com>
2022-08-03 03:25:32 +0200

    add users and machines

---
+++ tf/users.tf
@@ -0,0 +1,32 @@
+locals {
+  admins = toset([
+    "ian.davis",
+    "tom.hall"
+  ])
+}
+
+resource "aws_iam_user" "admin" {
+  for_each = local.admins
+  name     = each.key
+}
+
+resource "aws_iam_user_policy_attachment" "admin" {
+  for_each   = aws_iam_user.admin
+  user       = each.value.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+resource "aws_instance" "testbox" {
+  for_each      = local.admins
+  ami           = "ami-030802ad6e5ffb009"
+  instance_type = "m6i.xlarge"
+  key_name      = each.key
+  vpc_security_group_ids = [
+    aws_security_group.dealgood.id,
+    aws_security_group.allow_ssh.id
+  ]
+  subnet_id = module.vpc.public_subnets[0]
+  tags = {
+    Name = each.key
+  }
+}
