69880bb254db22acad5f7dcece7019121158091a
Ian Davis <nospam@iandavis.com>
2022-08-17 13:22:21 +0100

    Checkout and build branch

--- experiments/providerdelay/build.sh
+++ experiments/providerdelay/build.sh
@@ -2,10 +2,35 @@
 set -euo pipefail
 # set -x
 
+EXP_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+CONFIGS="$EXP_DIR/image/configs/*.sh"
+DOCKER_REPO=147263665150.dkr.ecr.eu-west-1.amazonaws.com
+KUBO_IMAGE_NAME=thunderdome:providerdelay-kubo
+KUBO_IMAGE=$DOCKER_REPO/$KUBO_IMAGE_NAME
 
-CONFIGS="image/configs/*.sh"
+WORKDIR=$(mktemp -d)
+if [ ! -e "$WORKDIR" ]; then
+    >&2 echo "Failed to create temp directory"
+    exit 1
+fi
 
-aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 147263665150.dkr.ecr.eu-west-1.amazonaws.com
+trap 'rm -rf "$WORKDIR"' EXIT
+
+echo "Using work directory $WORKDIR"
+cd "$WORKDIR"
+git clone https://github.com/iand/go-ipfs.git kubo
+
+cd kubo
+git switch feat/remove-provider-search-delay
+docker build -t $KUBO_IMAGE_NAME .
+
+aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin $DOCKER_REPO
+
+docker tag $KUBO_IMAGE_NAME $KUBO_IMAGE
+docker push $KUBO_IMAGE
+
+
+cd "$EXP_DIR"
 
 for cf in $CONFIGS
 do
@@ -15,12 +40,12 @@ do
  		name=${file%.sh}
  		# Build and push image
  		echo Building thunderdome:$name
-		echo docker build  --build-arg config=$file -t thunderdome:$name image/
-		echo docker tag thunderdome:$name 147263665150.dkr.ecr.eu-west-1.amazonaws.com/thunderdome:$name
-		echo docker push 147263665150.dkr.ecr.eu-west-1.amazonaws.com/thunderdome:$name
+		docker build --build-arg config=$file --build-arg kuboimage=$KUBO_IMAGE -t thunderdome:$name image/
+		docker tag thunderdome:$name $DOCKER_REPO/thunderdome:$name
+		docker push $DOCKER_REPO/thunderdome:$name
 	else
-    	echo "\"$cf\" was not a file"
-    	exit
+    	>&2 echo "\"$cf\" was not a file"
+    	exit 1
   	fi
 
 done
