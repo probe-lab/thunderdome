554cc18f80d7bd2b2f8e215a83b909f7ef9f8135
Tom Hall <thattommyhall@gmail.com>
2022-09-01 21:51:02 +0000

    Script to make Thunderdome-ready image from Kubo Dockerhub image

--- docker/from-kubo-image-tag/container-init.d/base.sh
+++ docker/from-kubo-image-tag/container-init.d/base.sh
@@ -4,18 +4,11 @@
 ## In Shellcheck, ash is an alias for dash, but busybox ash can do more than dash 
 ## https://github.com/koalaman/shellcheck/blob/master/src/ShellCheck/Data.hs#L134
 echo "setting config"
-ipfs config --json AutoNAT '{
-      "ServiceMode": "enabled",
-      "Throttle": {
-        "GlobalLimit": 0,
-        "Interval": "1m",
-        "PeerLimit": 3
-      }
-    }'
+ipfs config --json AutoNAT '{"ServiceMode": "disabled"}'
 
 ipfs config --json Datastore.BloomFilterSize '268435456'
 ipfs config --json Datastore.StorageGCWatermark 90
-ipfs config --json Datastore.StorageMax '"200GB"'
+ipfs config --json Datastore.StorageMax '"1000GB"'
 ipfs config --json Datastore.GCPeriod '"10m"'
 
 ipfs config --json Pubsub.StrictSignatureVerification false
