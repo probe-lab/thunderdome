ba1ac1cc9f42d4fed98bb736ac03b50d3bc323aa
Tom Hall <thattommyhall@gmail.com>
2022-09-05 19:40:55 +0000

    Allow building historic images
    Some config is ignored, but it works

--- docker/from-kubo-image-tag/build.sh
+++ docker/from-kubo-image-tag/build.sh
@@ -5,11 +5,14 @@ if [ -z "$1" ]; then
   echo "./build.sh v0.15.0"
   exit
 fi
+if [ -z "$REPO_NAME" ]; then
+  REPO_NAME=kubo
+fi
 set -euo pipefail
 TAG=$1
 IMAGE_NAME=thunderdome:kubo-$TAG
 ECR_REPO=147263665150.dkr.ecr.eu-west-1.amazonaws.com
-docker build -t "$IMAGE_NAME" --build-arg TAG="$TAG" .
+docker build -t "$IMAGE_NAME" --build-arg TAG="$TAG" --build-arg REPO_NAME="$REPO_NAME" .
 aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin "$ECR_REPO"
 docker tag "$IMAGE_NAME" "$ECR_REPO"/"$IMAGE_NAME"
 docker push "$ECR_REPO"/"$IMAGE_NAME"
